f708621f94fe1f0a6342fa5d759fba93d9a23c6b
OnkarParmar <onkar.parmar@gmail.com>
2021-05-15 16:11:39 +0530

    agteway for recommendation microservice

--- src/main/java/com/teamteach/gateway/api/DocumentationController.java
+++ src/main/java/com/teamteach/gateway/api/DocumentationController.java
@@ -21,6 +21,7 @@ public class DocumentationController implements SwaggerResourcesProvider {
                 resources.add(swaggerResource("users-docs", "/users-docs/v2/api-docs", "2.0"));
                 resources.add(swaggerResource("profiles-docs", "/profiles-docs/v2/api-docs", "2.0"));
                 resources.add(swaggerResource("journals-docs", "/journals-docs/v2/api-docs", "2.0"));
+                resources.add(swaggerResource("recommendations-docs", "/recommendations-docs/v2/api-docs", "2.0"));
                 return resources;
         }
 
